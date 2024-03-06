f4ff142724d7b3a08786782e6d775649d19057d4
Jordan Marchetto <jordan.marchetto@gmail.com>
2024-03-05 22:14:22 -0500

    refactor show view

--- app/controllers/workout_sets_controller.rb
+++ app/controllers/workout_sets_controller.rb
@@ -3,7 +3,7 @@ class WorkoutSetsController < ApplicationController
   def create
     #TODO: add param parsing
     exercise = Exercise.find(params[:exercise_id])
-    @set = WorkoutSet.new(exercise: exercise, type: params[:type], rep_count: params[:rep_count], rep_value: params[:rep_value] )
+    @set = WorkoutSet.new(exercise: exercise, type: params[:type], rep_count: params[:rep_count], rep_value: params[:rep_value], rpe: params[:rpe] )
 
     if @set.save
       redirect_to exercise
