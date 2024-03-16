9138c39d55d68be8b52ef24ba72ad6624e89c7ec
Jordan Marchetto <jordan.marchetto@gmail.com>
2024-03-16 11:55:41 -0400

    formatting

--- app/models/workout_set.rb
+++ app/models/workout_set.rb
@@ -1,6 +1,7 @@
 class WorkoutSet < ApplicationRecord
   belongs_to :exercise
 
+  # These should be defined in every child class
   NAME = "Workout Set Base Class"
   DESCRIPTION = "Base class for all other workout set subclasses."
 
@@ -16,17 +17,6 @@ class WorkoutSet < ApplicationRecord
   Dir["#{Rails.root}/app/models/workout_sets/*.rb"].each do |file|
     require_dependency file
   end
-
-  # # a human readable name for this type of workout set
-  # def self.name
-  #   raise NotImplementedError
-  # end
-
-  # # a human readable description for this type of workout set
-  # # it should explain how this type will be used
-  # def self.description
-  #   raise NotImplementedError
-  # end
 
   # we're using a bool db column, so true is up and false is down
   def timer_direction
