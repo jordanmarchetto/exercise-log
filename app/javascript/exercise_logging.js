9c56a4670742f50963b3c4915034a22534ae39f2
Jordan Marchetto <jordan.marchetto@gmail.com>
2024-03-25 21:22:48 -0400

    handle 'already defined' bug

--- app/javascript/exercise_logging.js
+++ app/javascript/exercise_logging.js
@@ -1,7 +1,7 @@
 //initialize the rep shortcut buttons
-const initRepShortcuts = () => {
-    const buttons = document.querySelectorAll('#shortcut-buttons button');
-    const repCount = document.querySelectorAll('#rep-count')[0];
+var initRepShortcuts = () => {
+    var buttons = document.querySelectorAll('#shortcut-buttons button');
+    var repCount = document.querySelectorAll('#rep-count')[0];
     buttons.forEach((btn)=> {
          btn.addEventListener('click', function(e, target) {
             e.preventDefault();
