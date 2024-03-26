5241430d6759b7431a81848dc0362c3a8e6fe42f
Jordan Marchetto <jordan.marchetto@gmail.com>
2024-03-25 22:17:41 -0400

    1rm calc

--- app/helpers/workout_sets_helper.rb
+++ app/helpers/workout_sets_helper.rb
@@ -1,2 +1,12 @@
 module WorkoutSetsHelper
+  def calc_1rm(sets)
+    best_max = 0
+    sets.each do |set|
+      next unless set.is_a?(WeightWorkoutSet) && set.rep_value
+
+      max = (set.rep_count * set.rep_value * 0.0333) + set.rep_value
+      best_max = max if max > best_max
+    end
+    best_max.to_i
+  end
 end
