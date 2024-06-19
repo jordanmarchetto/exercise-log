4bccf26699dca221e2935732dc346c5dd443b007
Jordan Marchetto <jordan.marchetto@gmail.com>
2024-06-19 16:56:07 -0400

    fix set logging

--- app/controllers/exercises_controller.rb
+++ app/controllers/exercises_controller.rb
@@ -47,11 +47,6 @@ class ExercisesController < ApplicationController
   private
 
   def exercise_params
-    filtered_params = params.require(:exercise).permit(:name, :description, :show_on_records, set_types: [])
-
-    if params[:set_types]
-      filtered_params[:set_types] = params[:set_types].keys
-    end
-    filtered_params
+    params.require(:exercise).permit(:name, :description, :show_on_records, set_types: [])
   end
 end
