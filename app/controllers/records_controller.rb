78b6d8b733661f44b1cdf968aefc9c4e25096f09
Jordan Marchetto <jordan.marchetto@gmail.com>
2024-06-19 18:26:28 -0400

    fix rounding

--- app/controllers/records_controller.rb
+++ app/controllers/records_controller.rb
@@ -3,8 +3,8 @@ class RecordsController < ApplicationController
     # fetch all exercises that we track totals, then calc the totals
     @exercises = Exercise.where(show_on_records: true)
 
-    real_total = @exercises.reduce(0) { |sum, exercise| sum + exercise.highest_weight }
-    estimated_total = @exercises.reduce(0) { |sum, exercise| sum + exercise.estimated_highest_set.estimated_max }
+    real_total = @exercises.reduce(0) { |sum, exercise| sum + exercise.highest_weight }.round(2)
+    estimated_total = @exercises.reduce(0) { |sum, exercise| sum + exercise.estimated_highest_set.estimated_max }.round(2)
     @totals = {real_total:, estimated_total:}
   end
 end
--- bin/docker-entrypoint.sh
+++ bin/docker-entrypoint.sh
@@ -1,8 +1,9 @@
 #!/bin/bash -e
 
 # If running the rails server then create or migrate existing database
-if [ "${1}" == "./bin/rails" ] && [ "${2}" == "server" ]; then
+# if [ "${1}" == "./bin/rails" ] && [ "${2}" == "server" ]; then
+  echo "running migrations"
   ./bin/rails db:prepare
-fi
+# fi
 
 exec "${@}"
