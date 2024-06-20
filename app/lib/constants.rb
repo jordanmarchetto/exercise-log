960772544af35ea3ddb1c21c6ece28795dbd87b1
Jordan Marchetto <jordan.marchetto@gmail.com>
2024-06-20 10:10:26 -0400

    use distance units constants

--- app/lib/constants.rb
+++ app/lib/constants.rb
@@ -1,4 +1,6 @@
 module Constants
   # application-wide constants here
   WEIGHT_UNIT = ENV['WEIGHT_UNIT'] || "lbs."
+  DISTANCE_UNITS = ['mi', 'km'].freeze
+  TIME_UNITS = ['ms', 's', 'min', 'hr'].freeze
 end
