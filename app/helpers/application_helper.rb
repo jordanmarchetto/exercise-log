1ccc72efb91adf4a460a62152d5e5b83c47d121d
Jordan Marchetto <jordan.marchetto@gmail.com>
2024-06-23 19:58:16 -0400

    add color helper method

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -16,6 +16,13 @@ module ApplicationHelper
     "<script>window.Constants = #{constants_to_json.html_safe};</script>".html_safe
   end
 
+  def lighten_color(hex_color, percentage)
+    hex_color = hex_color.gsub('#', '')
+    rgb = hex_color.scan(/../).map { |color| color.to_i(16) }
+    new_rgb = rgb.map { |c| (c + (255 - c) * (percentage / 100.0)).round }
+    "#%02x%02x%02x" % new_rgb
+  end
+
   private
 
   def constants_to_json
