e2526316be74276e67936b83be713a19b58ff8af
Jordan Marchetto <jordan.marchetto@gmail.com>
2024-07-05 13:43:43 -0400

    add pr to model

--- app/models/workout_sets/weight_workout_set.rb
+++ app/models/workout_sets/weight_workout_set.rb
@@ -1,6 +1,9 @@
 class WeightWorkoutSet < WorkoutSet
   # weight based set; ex: most barbell movements
+
+  before_save :check_for_pr
   alias_attribute :weight, :rep_value
+
   NAME = "Weight Set"
   DESCRIPTION = "Tracks a number of reps, how heavy each rep was, and RPE."
 
@@ -14,6 +17,19 @@ class WeightWorkoutSet < WorkoutSet
 
   def self.has_rpe?
     true
+  end
+
+  def is_estimated_pr?
+    !!self.data['is_estimated_pr']
+  end
+
+  def is_estimated_pr=(value)
+    self.data['is_estimated_pr'] = !!value
+  end
+
+  def check_for_pr
+    self.is_pr = true if self.weight >= self.exercise.highest_weight
+    self.is_estimated_pr =  true if self.estimated_max >= self.exercise.highest_estimated_weight
   end
 
   def estimated_max
