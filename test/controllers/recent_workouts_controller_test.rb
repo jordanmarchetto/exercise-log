67d6100239d2ed7e67d2738b4656d76d6de5cee6
Jordan Marchetto <jordan.marchetto@gmail.com>
2024-07-08 22:40:35 -0400

    remove minitest files

--- test/controllers/recent_workouts_controller_test.rb
+++
@@ -1,8 +0,0 @@
-require "test_helper"
-
-class RecentWorkoutsControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
-    get recent_workouts_index_url
-    assert_response :success
-  end
-end
